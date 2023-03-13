61735e4c5f1e02902ccda6dae2169619b6b9cfec
xln-tj <57608826+xlnn@users.noreply.github.com>
2023-03-13 20:11:06 +0800

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -2,7 +2,7 @@ import numpy as np
 import torch
 
 dependencies = ["torch", "numpy"]
-default_ckpt_url = "https://www.dropbox.com/s/j8ida1d86hy5tm4/waternet_exported_state_dict-daa0ee.pt?dl=1"
+default_ckpt_url = "https://www.dropbox.com/s/f5o8z3y6bjcpakl/last.pt?dl=0"
 
 
 def arr2ten_noeinops(arr):
